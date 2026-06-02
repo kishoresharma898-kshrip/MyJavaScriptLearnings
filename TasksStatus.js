793784ba5ad1508ea1eb2c7f4f1e5e9337ad3b69
kishoresharma898-kshrip <kishoresharma898@gmail.com>
2026-06-02 09:01:23 +0530

    Update TasksStatus.js
    Added class based object creation, short description field, proper conditional handling which display the messages on the console

--- TasksStatus.js
+++ TasksStatus.js
@@ -1,60 +1,90 @@
-// Overdue records. A small ServiceNow requirement to complete an exercise. Developed and Tested on 2026-06-01 09:11:35 UTC
-var odRecord1 = {
-    id: "NITASK003001",
-    assignee: "Beth Anglin",
-    dueDate: new Date("2026-05-25T13:55:00"),
-    severity: 3,
-    state: 1
+// Overdue records. A small ServiceNow requirement to complete an exercise. Developed and Tested on 2026-06-02 03:28:18 UTC
+// Classical object based creation
+class Record {
+    constructor (id, assignee, dueDate, severity, state, shortDescription) {
+        this.id = id;
+        this.assignee = assignee
+        this.dueDate = dueDate
+        this.severity = severity
+        this.state = state
+        this.shortDescription = shortDescription
+    }
 }
-var odRecord2 = {
-    id: "NITASK003002",
-    assignee: "Beth Anglin",
-    dueDate: new Date("2026-06-07T15:36:00"),
-    severity: 4,
-    state: 2
-}
-var odRecord3 = {
-    id: "NITASK003003",
-    assignee: "Beth Anglin",
-    dueDate: new Date("2026-05-12T20:53:00"),
-    severity: 3,
-    state: 3
-}
-var odRecord4 = {
-    id: "NITASK003004",
-    assignee: "Fred Luddy",
-    dueDate: new Date("2026-06-01T20:53:00"),
-    severity: 2,
-    state: 1
-}
-var odRecord5 = {
-    id: "NITASK003005",
-    assignee: "Ashley Parker",
-    dueDate: new Date("2026-06-01T20:53:00"),
-    severity: 4,
-    state: 2
-}
-var odRecord6 = {
-    id: "NITASK003006",
-    assignee: "Andrew Och",
-    dueDate: new Date("2026-06-01T20:53:00"),
-    severity: 2,
-    state: 2
-}
-var odRecord7 = {
-    id: "NITASK003007",
-    assignee: "Abraham Lincoln",
-    dueDate: new Date("2026-06-01T20:53:00"),
-    severity: 1,
-    state: 1
-}
-var odRecord8 = {
-    id: "NITASK003008",
-    assignee: "Abraham Lincoln",
-    dueDate: new Date("2026-06-01T20:53:00"),
-    severity: 2,
-    state: 1
-}
+
+var odRecord1 = new Record("NITASK003001", "Beth Anglin",new Date("2026-05-25T13:55:00"), 3, 1, "Overdue")
+var odRecord2 = new Record("NITASK003002", "Beth Anglin",new Date("2026-06-07T15:36:00"), 4, 2, "Not Overdue")
+var odRecord3 = new Record("NITASK003003", "Beth Anglin",new Date("2026-05-12T20:53:00"), 3, 3, "Due date in past but record is closed")
+var odRecord4 = new Record("NITASK003004", "Fred Luddy",new Date("2026-06-02T20:53:00"), 2, 1, "Overdue in 24hrs")
+var odRecord5 = new Record("NITASK003005", "Ashley Parker",new Date("2026-06-02T20:53:00"), 4, 2, "Open for now. Will be closed after 24hrs")
+var odRecord6 = new Record("NITASK003006", "Andrew Och",new Date("2026-06-02T20:53:00"), 2, 2, "Should be closed in 24hrs")
+var odRecord7 = new Record("NITASK003007", "Abraham Lincoln",new Date("2026-06-02T20:53:00"), 1, 1, "Overdue by 24hrs")
+var odRecord8 = new Record("NITASK003008", "Abraham Lincoln",new Date("2026-06-02T20:53:00"), 2, 1, "Will be closed after 24hrs")
+
+// Traditional object based creation.
+// var odRecord1 = {
+//     id: "NITASK003001",
+//     assignee: "Beth Anglin",
+//     dueDate: new Date("2026-05-25T13:55:00"),
+//     severity: 3,
+//     state: 1,
+//     shortDescription: "Overdue"
+// }
+// var odRecord2 = {
+//     id: "NITASK003002",
+//     assignee: "Beth Anglin",
+//     dueDate: new Date("2026-06-07T15:36:00"),
+//     severity: 4,
+//     state: 2,
+//     shortDescription: "Not Overdue"
+// }
+// var odRecord3 = {
+//     id: "NITASK003003",
+//     assignee: "Beth Anglin",
+//     dueDate: new Date("2026-05-12T20:53:00"),
+//     severity: 3,
+//     state: 3,
+//     shortDescription: "Due date in past but record is closed"
+// }
+// var odRecord4 = {
+//     id: "NITASK003004",
+//     assignee: "Fred Luddy",
+//     dueDate: new Date("2026-06-02T20:53:00"),
+//     severity: 2,
+//     state: 1,
+//     shortDescription: "Overdue in 24hrs"
+// }
+// var odRecord5 = {
+//     id: "NITASK003005",
+//     assignee: "Ashley Parker",
+//     dueDate: new Date("2026-06-02T20:53:00"),
+//     severity: 4,
+//     state: 2,
+//     shortDescription: "Open for now. Will be closed after 24hrs"
+// }
+// var odRecord6 = {
+//     id: "NITASK003006",
+//     assignee: "Andrew Och",
+//     dueDate: new Date("2026-06-02T20:53:00"),
+//     severity: 2,
+//     state: 2,
+//     shortDescription: "Should be closed in 24hrs"
+// }
+// var odRecord7 = {
+//     id: "NITASK003007",
+//     assignee: "Abraham Lincoln",
+//     dueDate: new Date("2026-06-02T20:53:00"),
+//     severity: 1,
+//     state: 1,
+//     shortDescription: "Overdue by 24hrs"
+// }
+// var odRecord8 = {
+//     id: "NITASK003008",
+//     assignee: "Abraham Lincoln",
+//     dueDate: new Date("2026-06-02T20:53:00"),
+//     severity: 2,
+//     state: 1,
+//     shortDescription: "Will be closed after 24hrs"
+// }
 
 var overdueRecords = [odRecord1, odRecord2, odRecord3, odRecord4, odRecord5, odRecord6, odRecord7, odRecord8]
 var pastOverdues = []
@@ -83,6 +113,8 @@ if (pastOverdues.length != 0) {
     pastOverdues.forEach((item) => {
         console.log("Task with id " + item.id +  " is overdue.")
     })
+} else {
+    console.log("No past overdue tasks.")
 }
 console.log()
 if (overduesByTomorrow.length != 0) {
@@ -90,6 +122,8 @@ if (overduesByTomorrow.length != 0) {
     overduesByTomorrow.forEach((item) => {
         console.log("Task with id " + item.id +  " is going to be overdue by tomorrow.")
     })
+} else {
+    console.log("No tasks are overdue by tomorrow.")
 }
 console.log()
 if (closed.length != 0) {
@@ -97,6 +131,8 @@ if (closed.length != 0) {
     closed.forEach((item) => {
         console.log("Task with id " + item.id +  " is closed.")
     })
+} else {
+    console.log("No closed tasks.")
 }
 console.log()
 if (soonOverdues.length != 0) {
@@ -104,6 +140,8 @@ if (soonOverdues.length != 0) {
     soonOverdues.forEach((item) => {
         console.log("Task with id " + item.id +  " is open and going to be overdue soon. Overdue by " + item.dueDate)
     })
+} else {
+    console.log("No tasks for overdue sooner.")
 }
 
 // Output:
